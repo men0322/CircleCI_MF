17e07c88fe86f6b1d6656acd98a5269bce94baeb
Nguyen Hung <nguyen.hung@moneyforward.vn>
2019-02-11 14:42:06 +0700

    Test CircleCI

---
+++ CircleCI_MF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircleCI_MF
+//
+//  Created by Nguyen Hung on 2/11/19.
+//  Copyright © 2019 Nguyen Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
